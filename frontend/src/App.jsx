f7affd629c356a6fc162a8dc6ffa248446d69457
Burhan Sheikh <burhan@Burhans-MacBook-Air.local>
2025-09-27 14:17:45 +0530

    DONEEE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Signup from './components/Signup';
-import Login from './components/Login';
-import Home from './components/Home'
-import Index from './components/Index';
+import React, { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Index from "./components/Index";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   return (
     <>
       <Router>
-          <Routes>
-              <Route  path="/" element={<Home />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />}/>
-              <Route path="/index" element={<Index />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/index"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+                <Index />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
